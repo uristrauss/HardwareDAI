732307e827244df8774644e2aff3a018e9b7f52f
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-01 10:37:44 -0300

    emergencias

--- Emergencia.js
+++ Emergencia.js
@@ -1,8 +1,82 @@
-import react from "react";
-  import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
   const Emergencia = () =>{
 
+    const [{ x, y, z }, setData] = useState({
+      x: 0,
+      y: 0,
+      z: 0,
+    });
+    const [subscription, setSubscription] = useState(null);
+  
+    const _slow = () => Accelerometer.setUpdateInterval(1000);
+    const _fast = () => Accelerometer.setUpdateInterval(16);
+  
+    const _subscribe = () => {
+      setSubscription(Accelerometer.addListener(setData));
+    };
+  
+    const _unsubscribe = () => {
+      subscription && subscription.remove();
+      setSubscription(null);
+    };
+  
+    useEffect(() => {
+      _subscribe();
+      return () => _unsubscribe();
+    }, []);
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
+        <Text style={styles.text}>x: {x}</Text>
+        <Text style={styles.text}>y: {y}</Text>
+        <Text style={styles.text}>z: {z}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+            <Text>{subscription ? 'On' : 'Off'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+            <Text>Slow</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_fast} style={styles.button}>
+            <Text>Fast</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+
 
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff', // Set your desired background color
+    },
+    text: {
+      fontSize: 18,
+      marginBottom: 10,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 20,
+    },
+    button: {
+      backgroundColor: '#DDDDDD',
+      padding: 10,
+      margin: 5,
+      borderRadius: 5,
+    },
+    middleButton: {
+      backgroundColor: '#AAAAAA', // Change the color of the middle button
+    },
+  });
+
   export default Emergencia;
